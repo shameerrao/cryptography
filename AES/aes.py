1a1a98abc9aca06f8c30747887f23d1f5295483b
Shameer Rao <73150291+shameerrao@users.noreply.github.com>
2025-03-04 18:42:36 -0500

    merge utils

--- AES/aes.py
+++ AES/aes.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
-from utils import *
 from sboxes import Sbox, Sbox_inv
+
+def double(a):
+    if (a&0x80)>>7==1:
+        return (a<<1)^0x11b # operation known as "xtime" in initial AES proposal
+    else:
+        return a*2
+
+def rotWord(word):
+    # word = [a b c d]; return [b c d a]
+    assert len(word)==4
+    return word[1:]+[word[0]]
+
+def subWord(word):
+    # word = [a b c d]; return [S(a) S(b) S(c) S(d)]
+    assert len(word)==4
+    return [Sbox[word[i]] for i in range(4)]
 
 def subBytes(state):
     return [[Sbox[state[i][j]] for j in range(4)] for i in range(4)]
